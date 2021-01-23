cf7b4cef2af31bbc8656da9d3ba223041d0545ad
Kidzdev <34876175+Kidzdev@users.noreply.github.com>
2021-01-23 13:04:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ from os import environ
 from pyrogram import filters
 
 
-API_ID = environ.get('API_ID') # Get these two from https://my.telegram.org eg:1234567
-API_HASH = environ.get('API_HASH') # Get these two from https://my.telegram.org.  eg:ab1c23def45fg67890h123i45678j9kl
-TOKEN = environ.get('TOKEN')   ## Get this from @Botfather eg:1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI
-SUDO_USERS = environ.get('SUDO_USERS') # The IDs of the users which can stream, skip, pause and change volume. eg: 625145821
-GROUP = environ.get('GROUP') # The ID of the group where your bot streams. eg:-1001402753006
-MONGO_DB_URI = environ.get('MONGO_DB_URI') # Your mongodb uri. eg:mongodb+srv://username:password@vcpb.opda3.mongodb.net/<dbname>?retryWrites=true&w=majority
-USERS_MUST_JOIN = environ.get('USERS_MUST_JOIN', 'False') # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
-LANG = environ.get('LANG', 'en') # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
-DUR_LIMIT = environ.get('DUR_LIMIT', 10) # Max video duration allowed for user downloads in minutes
+API_ID = environ.get('1331889') # Get these two from https://my.telegram.org eg:1234567
+API_HASH = environ.get('b6f525900fd20d4282116c6fe13094ac') # Get these two from https://my.telegram.org.  eg:ab1c23def45fg67890h123i45678j9kl
+TOKEN = environ.get('1539987153:AAHaJz_OCFJXTT-Oig_jNryHkwn9FjvUJ80')   ## Get this from @Botfather eg:1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI
+SUDO_USERS = environ.get('879481389') # The IDs of the users which can stream, skip, pause and change volume. eg: 625145821
+GROUP = environ.get('-1001474831419') # The ID of the group where your bot streams. eg:-1001402753006
+MONGO_DB_URI = environ.get('mongodb+srv://edi123:edi123@cluster0.r463p.mongodb.net/dbtest?retryWrites=true&w=majority') # Your mongodb uri. eg:mongodb+srv://username:password@vcpb.opda3.mongodb.net/<dbname>?retryWrites=true&w=majority
+USERS_MUST_JOIN = environ.get('USERS_MUST_JOIN', 'True') # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
+LANG = environ.get('LANG', 'id') # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
+DUR_LIMIT = environ.get('DUR_LIMIT', 20) # Max video duration allowed for user downloads in minutes
 
 
 
