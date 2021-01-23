3df475ec627c18832c0df5d9681e96020593380d
Kidzdev <34876175+Kidzdev@users.noreply.github.com>
2021-01-23 13:13:44 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $UID -eq 0 ]; then
-user=$edi #Make change here if you changed username
+user=$vcpb #Make change here if you changed username
 exec su "$user" "$0" -- "$@"
 fi
 echo "This will be run from user $UID"
